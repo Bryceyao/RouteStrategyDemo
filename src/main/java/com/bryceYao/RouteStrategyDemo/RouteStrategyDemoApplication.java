d216ade2065e5d65803dd3e27f7ca2ae8820442c
Bryceyao <bryce.yao@wolaidai.com>
2017-09-05 15:04:23 +0800

    a

---
+++ src/main/java/com/bryceYao/RouteStrategyDemo/RouteStrategyDemoApplication.java
@@ -0,0 +1,12 @@
+package com.bryceYao.RouteStrategyDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteStrategyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteStrategyDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bryceYao/RouteStrategyDemo/RouteStrategyDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bryceYao.RouteStrategyDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RouteStrategyDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
